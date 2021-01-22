ae7d4c070c04db244afa0f5c1520651c80511f59
Yauhen <example@gmail.com>
2021-01-22 23:44:18 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,47 @@
+"use strict";
+// let number = 5;
+// const leftBorderWidth = 1;
+
+// number = 12;
+// console.log(number);
+
+// let number = 4.6;
+
+// const person = 'Alex';
+
+// const bool = true; 
+
+// Объект
+// const obj = {
+//     name: 'Yauhen',
+//     age: 25,
+//     isMarried: false
+// };
+// console.log(obj.age);
+// console.log(obj["age"]);
+
+// Массив
+// let arr = ['plum.png', 1, 'orange.jpg', {}];
+// console.log(arr[0]);
+
+// let result = confirm('Are you here?');
+// console.log(result);
+
+// let answer = prompt('Вам есть 18 лет?', '18');
+// console.log(typeof(answer));
+
+//Запись в Масив
+// const answers = [];
+// answers[0] = prompt('Ваше имя', '');
+// answers[1] = prompt('Ваша фамилия', '');
+// answers[2] = prompt('Ваш возраст', '');
+// document.write(answers);
+
+//Интерполяция
+// const category = 'toys';
+//  console.log(`http://someurl.com/${category}/5`);
+ const userName = 'Ilai';
+ alert(`Hi, ${userName}`);
+
+//Операторы
+// ==, ===, @, &&, ||, !=, !==, 
