e7a6f0744cef3fdf1c0ff6d1f1ca86f491f47aa5
Yauhen <example@gmail.com>
2021-01-24 13:53:04 +0100

    add new task(movie and rate)

--- js/script.js
+++ js/script.js
@@ -40,8 +40,29 @@
 //Интерполяция
 // const category = 'toys';
 //  console.log(`http://someurl.com/${category}/5`);
- const userName = 'Ilai';
- alert(`Hi, ${userName}`);
+//  const userName = 'Ilai';
+//  alert(`Hi, ${userName}`);
 
 //Операторы
-// ==, ===, @, &&, ||, !=, !==, 
+// ==, ===, @, &&, ||, !=, !==,
+
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+console.log(numberOfFilms);
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt('Один из просмотренных фильмов?', ''),
+      b = prompt('Оцените его', ''),
+      c = prompt('Один из просмотренных фильмов?', ''),
+      d = prompt('Оцените его', '');
+
+    personalMovieDB.movies[a] = b;
+    personalMovieDB.movies[c] = d;
+
+    console.log(personalMovieDB);
